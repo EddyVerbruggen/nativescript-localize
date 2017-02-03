9638f8f01e29a703677dbe075c536e80e0d2490b
Ludovic Fabrèges <ludovic.fabreges@gmail.com>
2017-02-03 11:09:32 +0100

    Tabs to spaces

--- hooks/before-prepare.ts
+++ hooks/before-prepare.ts
@@ -6,22 +6,22 @@ import { BeforePrepareAndroid } from "./before-prepare.android";
 import { BeforePrepareIOS } from "./before-prepare.ios";
 
 export = function(logger: ILogger, platformsData: IPlatformsData, projectData: IProjectData, hookArgs: any) {
-	const platformName = hookArgs.platform.toLowerCase();
-	const platformData = platformsData.getPlatformData(platformName);
+  const platformName = hookArgs.platform.toLowerCase();
+  const platformData = platformsData.getPlatformData(platformName);
 
-	let beforePreparePlatform: BeforePrepareCommon;
+  let beforePreparePlatform: BeforePrepareCommon;
 
-	if (platformName === "android") {
-		beforePreparePlatform = new BeforePrepareAndroid(logger, platformData, projectData);
-	} else if (platformName === "ios") {
-		beforePreparePlatform = new BeforePrepareIOS(logger, platformData, projectData);
-	} else {
-		logger.warn(`Platform '${platformName}' isn't supported: skipping localization`);
-		return;
-	}
+  if (platformName === "android") {
+    beforePreparePlatform = new BeforePrepareAndroid(logger, platformData, projectData);
+  } else if (platformName === "ios") {
+    beforePreparePlatform = new BeforePrepareIOS(logger, platformData, projectData);
+  } else {
+    logger.warn(`Platform '${platformName}' isn't supported: skipping localization`);
+    return;
+  }
 
-	beforePreparePlatform
-		.on(BeforePrepareCommon.RESOURCE_CHANGED_EVENT, () => hookArgs.changesInfo.appResourcesChanged = true)
-		.on(BeforePrepareCommon.CONFIGURATION_CHANGED_EVENT, () => hookArgs.changesInfo.configChanged = true)
-		.run();
+  beforePreparePlatform
+    .on(BeforePrepareCommon.RESOURCE_CHANGED_EVENT, () => hookArgs.changesInfo.appResourcesChanged = true)
+    .on(BeforePrepareCommon.CONFIGURATION_CHANGED_EVENT, () => hookArgs.changesInfo.configChanged = true)
+    .run();
 }
