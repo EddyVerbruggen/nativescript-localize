5f6989ab8e4da0fbc8b2ae4552e9544427a96297
Ludovic Fabrèges <ludovic.fabreges@gmail.com>
2017-05-11 10:42:36 +0200

    Add Angular demo

---
+++ demo/angular/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
+import { NativeScriptLocalizeModule } from "nativescript-localize";
+import { NativeScriptModule } from "nativescript-angular/nativescript.module";
+
+import { AppComponent } from "./app.component";
+
+@NgModule({
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+  imports: [
+    NativeScriptModule,
+    NativeScriptLocalizeModule
+  ],
+  schemas: [NO_ERRORS_SCHEMA],
+})
+export class AppModule {}
