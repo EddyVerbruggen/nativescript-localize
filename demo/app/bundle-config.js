8a8de0b8271fa57b86a5bcc47bd5dd4f181c1f18
Ludovic Fabrèges <ludovic.fabreges@gmail.com>
2018-11-25 14:13:33 +0100

    Remove unused bundle-config

--- demo/app/bundle-config.js
+++
@@ -1,5 +0,0 @@
-if (global.TNS_WEBPACK) {
-  require("bundle-entry-points");
-  const context = require.context("~/", true, /(page|fragment)\.(xml|css|js|ts|scss|less|sass)$/);
-  global.registerWebpackModules(context);
-}
